e582dc74a4fabbefbbe9a5e63ab4e6a82228e2e7
Viet <phanminh.0970@gmail.com>
2022-08-27 16:02:13 +0300

    Example004

---
+++ Example004/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("What is ur name");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "viet")
+{
+    Console.Write("Welcome to the Rift, ");
+    Console.Write(username);
+}
+else
+{
+    Console.Write("We do not know you");
+}
