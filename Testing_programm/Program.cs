fd5192324220a06432b37e3af37f60b2c6cb09ba
Viet <phanminh.0970@gmail.com>
2022-08-27 16:11:57 +0300

    TESTING

---
+++ Testing_programm/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 1; 
+int b = 8;
+int c = 3;
+int d = 2;
+int e = 6;
+
+int max = a;
+
+if ( max < b ) max = b;
+if ( max < c ) max = c;
+if ( max < d) max = d;
+if (max < e) max = e;
+
+Console.WriteLine(max);
